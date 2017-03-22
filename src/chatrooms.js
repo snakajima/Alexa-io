256f12e4e7aa79451f60bbceeddaad64205279a2
SATOSHI NAKAJIMA <satoshi.nakajima@gmail.com>
2017-03-22 15:39:54 -0700

    chatroom.js from remoteio

---
+++ src/chatrooms.js
@@ -0,0 +1,18 @@
+module.exports = function(io) {
+    io.on('connection', function(socket) {
+      console.log('connection');
+      
+      socket.on('/room/join', function(data) {
+        console.log('/room/join', data.name);
+        socket.join(data.name);
+        io.sockets.adapter.sids[socket.id].ROOM = data.name;
+        socket.emit('/room/join/success');
+      });
+      
+      socket.on('/room/message', function(data) {
+        var room = io.sockets.adapter.sids[socket.id].ROOM;
+        console.log('message', room, data);
+        io.sockets.in(room).emit('/room/message', data);
+      });
+    });
+}
---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const serverPort = 8080;
+var server = require('http').createServer(app);
+
+server.listen(serverPort, () => {
+   console.log('listening on *:' + serverPort);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: path.join(__dirname, '../html') });
+});
+app.get('/html/:filename', (req, res) => {
+    res.sendFile(req.params.filename, { root: path.join(__dirname, '../html') });
+});
+app.get('/css/:filename', (req, res) => {
+    res.sendFile(req.params.filename, { root: path.join(__dirname, '../css') });
+});
+app.get('/js/:filename', (req, res) => {
+    res.sendFile(req.params.filename, { root: path.join(__dirname, '../js') });
+});
+
+const io = require('socket.io')(server);
+const chatrooms = require('./chatrooms');
+chatrooms(io);
