b82304b1319c92f2d5fc334e8ce8b5eef086df0d
SATOSHI NAKAJIMA <satoshi.nakajima@gmail.com>
2017-03-23 16:33:27 -0700

    only intent

--- js/remote.io.js
+++ js/remote.io.js
@@ -9,18 +9,7 @@
         return (_room==undefined) ? "Lobby" : _room;
     })();
     var remoteIO = {
-      setCommands:function(_verbs) { verbs = _verbs; },
-      // to be overridden by the app
-      onFocus:function(focus) {},
-      onCommand:function(command) {},
-      onScene:function(scene) {
-        console.log("scene =", scene.name, scene.path);
-        if (scene.path != undefined) {
-            window.location.href = scene.path;
-        }
-      },
-      handlePan:function(state, pos, translate) {},
-      handlePinch:function(state, pos, scale) {},
+        onIntent:function(intent) { console.log('intent'); }
     };
 
     var socket = io({});
@@ -30,36 +19,12 @@
     });
     socket.on('/room/join/success', function(data) {
         console.log("/room/join/success");
-        socket.emit('/room/message', {cmd:"advertise", guid:guid, verbs:verbs});
     });
     socket.on('/room/message', function(data) {
         console.log("/room/message", data);
         switch(data.cmd) {
-        case 'scan':
-            socket.emit('/room/message', {cmd:"advertise", guid:guid, verbs:verbs});
-            break;
-        case 'focus':
-            if (data.guid == guid) {
-                remoteIO.onFocus(data.focus);
-            }
-            break;
-        case 'switch':
-            if (data.guid == guid) {
-                localStorage.setItem("room", data.room);
-                socket.emit('/room/join', {name: data.room});
-            }
-            break;
-        case 'custom':
-            remoteIO.onCommand(data.verb);
-            break;
-        case 'scene':
-            remoteIO.onScene(data.scene);
-            break;
-        case 'pan':
-            remoteIO.handlePan(data.state, data.pos, data.tx);
-            break;
-        case 'pinch':
-            remoteIO.handlePinch(data.state, data.pos, data.scale);
+        case 'intent':
+            remoteIO.onIntent(data.intent);
             break;
         }
     });
